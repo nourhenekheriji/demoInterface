5e68fb7909dab0f9a08cb0dc80c349fa2ef2e7bf
Nourhene KHERIJI <nourhene.kheriji@talan.com>
2023-03-30 16:37:13 +0200

    initialisation

---
+++ src/main/java/com/example/talan/demoInterface/DemoInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.talan.demoInterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInterfaceApplication.class, args);
+	}
+
+}
