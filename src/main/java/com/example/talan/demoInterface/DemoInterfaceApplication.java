54a7bd0ec87c9bc61b9a08e66fed3720712accb8
NourheneKHERIJI <nourhene.kheriji@esprit.tn>
2023-03-30 16:57:37 +0200

    create get Mapping method -closes #1

--- src/main/java/com/example/talan/demoInterface/DemoInterfaceApplication.java
+++ src/main/java/com/example/talan/demoInterface/DemoInterfaceApplication.java
@@ -2,12 +2,23 @@ package com.example.talan.demoInterface;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api")
 public class DemoInterfaceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoInterfaceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoInterfaceApplication.class, args);
+    }
+
+
+    @GetMapping("/showMessage")
+    public String getSrtring() {
+        return "hello word";
+    }
 
 }
